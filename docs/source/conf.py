aca37b7d31383918f8d92e0263c3ccd2cc30bf8f
prakaa <abiprakash007@gmail.com>
2022-07-05 12:08:41 +1000

    configure for autodocs and manage extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,9 @@ release = '0.1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode', 'sphinx.ext.githubpages', 'sphinx.ext.napoleon',
+extensions = [
+'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.ifconfig', 
+'sphinx.ext.viewcode', 'sphinx.ext.githubpages', 'sphinx.ext.napoleon', 'sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -41,6 +43,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# -- Autodoc options ---------------------------------------------------------
+
+autodoc_mock_imports = ["attrs", "beautifulsoup4", "nemosis", "pandas", "requests", "xarray"]
 
 # -- Options for HTML output -------------------------------------------------
 
