12100a2c6be5b43a8799f168b93656765b9a34d6
prakaa <abiprakash007@gmail.com>
2022-07-26 19:23:36 +1000

    black formatting

--- src/nemseer/dl_helpers/urls.py
+++ src/nemseer/dl_helpers/urls.py
@@ -1,10 +1,5 @@
-
 # Wholesale electricity data archive base URL
-MMSDM_ARCHIVE_URL = (
-    "http://www.nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/"
-)
+MMSDM_ARCHIVE_URL = "http://www.nemweb.com.au/Data_Archive/Wholesale_Electricity/MMSDM/"
 
 # MTPASA DUID Availability
-MTPASA_DUID_URL = (
-    "http://nemweb.com.au/Reports/Current/MTPASA_DUIDAvailability/"
-)
+MTPASA_DUID_URL = "http://nemweb.com.au/Reports/Current/MTPASA_DUIDAvailability/"
