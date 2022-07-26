45160094f31a4ac6eb49c593fbb829247ca33236
prakaa <abiprakash007@gmail.com>
2022-07-26 14:45:03 +1000

    black formatting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/'))
+
+sys.path.insert(0, os.path.abspath("../../src/"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'NEMSEER'
-copyright = '2022, Abhijith (Abi) Prakash'
-author = 'Abhijith (Abi) Prakash'
+project = "NEMSEER"
+copyright = "2022, Abhijith (Abi) Prakash"
+author = "Abhijith (Abi) Prakash"
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = "0.1.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,13 +32,18 @@ release = '0.1.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.napoleon', 'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-    'sphinx.ext.viewcode', 'sphinx.ext.githubpages', 'myst_parser'
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.doctest",
+    "sphinx.ext.todo",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.githubpages",
+    "myst_parser",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -46,7 +52,7 @@ exclude_patterns = []
 
 
 # -- Formats for MyST --------------------------------------------------------
-source_suffix = ['.rst', '.md']
+source_suffix = [".rst", ".md"]
 
 # --  Napoleon options--------------------------------------------------------
 # use the :param directive
@@ -62,7 +68,7 @@ autodoc_typehints = "both"
 autoclass_content = "class"
 
 # --  Intersphinx options-----------------------------------------------------
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+intersphinx_mapping = {"python": ("https://docs.python.org/3", None)}
 
 # --  MyST options------------------------------------------------------------
 
@@ -76,9 +82,9 @@ myst_enable_extensions = [
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'furo'
+html_theme = "furo"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
