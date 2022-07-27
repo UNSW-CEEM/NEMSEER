f7fcabb983bdad3d3d5b3551310073cbaa7a47d3
prakaa <abiprakash007@gmail.com>
2022-07-27 16:20:12 +1000

    configure module logger

--- src/nemseer/__init__.py
+++ src/nemseer/__init__.py
@@ -1,4 +1,12 @@
 # read version from installed package
+import sys
 from importlib.metadata import version
 
 __version__ = version("nemseer")
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+logging.basicConfig(
+    stream=sys.stdout, level=logging.INFO, format="%(levelname)s: %(message)s"
+)
