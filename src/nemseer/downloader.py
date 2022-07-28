879f4106a1d66d3fb2d68c74a988fe87c80086df
prakaa <abiprakash007@gmail.com>
2022-07-28 20:20:26 +1000

    download zip

--- src/nemseer/downloader.py
+++ src/nemseer/downloader.py
@@ -1,10 +1,19 @@
+import logging
 from datetime import datetime
 from typing import List, Optional
 
 from attrs import define, field
+from dateutil import rrule
 
-from .dl_helpers.functions import get_tables_for_yearmonths
+from .dl_helpers.functions import (
+    _construct_sqlloader_forecastdata_url,
+    get_historical_forecast_tables,
+    get_sqlloader_filesize,
+    get_unzipped_csv,
+)
 from .loader import Loader
+
+logger = logging.getLogger(__name__)
 
 
 def _enumerate_tables(tables: List[str], table_str: str, range_to: int):
@@ -33,7 +42,7 @@ def _validate_tables_on_forecast_start(instance, attribute, value):
     Data SQL Loader for the month and year of forecast_start.
     """
     start_dt = instance.forecast_start
-    tables = get_tables_for_yearmonths(
+    tables = get_historical_forecast_tables(
         start_dt.year, start_dt.month, instance.forecast_type
     )
     if not set(value).issubset(set(tables)):
@@ -65,3 +74,17 @@ class ForecastTypeDownloader:
             tables=tables,
             raw_cache=loader.raw_cache,
         )
+
+    def download_zip_to_parquet(self):
+        intervening_dates = rrule.rrule(
+            rrule.MONTHLY, dtstart=self.forecast_start, until=self.forecast_end
+        )
+        for table in self.tables:
+            for date in intervening_dates:
+                (year, month) = (date.year, date.month)
+                url = _construct_sqlloader_forecastdata_url(
+                    year, month, self.forecast_type, table
+                )
+                size = get_sqlloader_filesize(year, month, self.forecast_type, table)
+                logger.info(f"Downloading {table} for {month}/{year}: {size} MB")
+                get_unzipped_csv(url, self.raw_cache)
