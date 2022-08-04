5c3095aaa410f3876adfcb38bdc0bbae53ef069b
prakaa <abiprakash007@gmail.com>
2022-08-04 23:14:41 +1000

    url constuctor test

---
+++ tests/test_dl_helpers.py
@@ -0,0 +1,11 @@
+from nemseer.dl_helpers.data import MMSDM_ARCHIVE_URL
+from nemseer.dl_helpers.functions import _construct_sqlloader_forecastdata_url
+
+
+def test_standard_sqlloader_url():
+    url = _construct_sqlloader_forecastdata_url(2021, 2, "STPASA", "REGIONSOLUTION")
+    assert url == (
+        MMSDM_ARCHIVE_URL
+        + "2021/MMSDM_2021_02/MMSDM_Historical_Data_SQLLoader/DATA/"
+        + "PUBLIC_DVD_STPASA_REGIONSOLUTION_202102010000.zip"
+    )
