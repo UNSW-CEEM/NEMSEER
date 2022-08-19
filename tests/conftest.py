bf5470302fcf2cb8e04ac34ae072edd6868a4643
abiprakash007@gmail.com <abiprakash007@gmail.com>
2022-08-19 12:50:02 +1000

    add download file to cache fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 
 from nemseer.downloader import get_sqlloader_years_and_months
+from nemseer.nemseer import download_raw_data
+from nemseer.query import Query
 
 
 @pytest.fixture(scope="module")
@@ -19,3 +21,32 @@ def valid_datetimes():
     forecasted_start = "2021/02/08 00:00"
     forecasted_end = "2021/02/08 23:55"
     return forecast_start, forecast_end, forecasted_start, forecasted_end
+
+
+@pytest.fixture(scope="module")
+def download_file_to_cache(tmp_path_factory, valid_datetimes):
+    (
+        forecast_start,
+        forecast_end,
+        forecasted_start,
+        forecasted_end,
+    ) = valid_datetimes
+    tmp_dir = tmp_path_factory.mktemp("raw_cache")
+    download_raw_data(
+        forecast_start,
+        forecast_end,
+        forecasted_start,
+        forecasted_end,
+        "MTPASA",
+        "REGIONRESULT",
+        tmp_dir,
+    )
+    return Query.initialise(
+        forecast_start,
+        forecast_end,
+        forecasted_start,
+        forecasted_end,
+        "MTPASA",
+        "REGIONRESULT",
+        tmp_dir,
+    )
