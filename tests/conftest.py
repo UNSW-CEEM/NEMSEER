f5c90cd78cb3685c47c3bbc57617059e4857f125
prakaa <abiprakash007@gmail.com>
2022-08-30 14:53:57 +1000

    change test date generator to generate from nov 2012

--- tests/conftest.py
+++ tests/conftest.py
@@ -65,7 +65,7 @@ def _gen_datetime():
     """
     min_year = 2012
     max_year = datetime.now().year
-    start = datetime(min_year, 1, 1, 00, 00, 00)
+    start = datetime(min_year, 11, 1, 00, 00, 00)
     years = max_year - min_year + 1
     end = start + timedelta(days=365 * years)
     return start + (end - start) * random.random()
