f1030c6255972ffc919810da0c134c0e74a8af7d
prakaa <abiprakash007@gmail.com>
2022-08-30 15:31:07 +1000

    min and max year manually set to years to avoid download hangs

--- tests/conftest.py
+++ tests/conftest.py
@@ -63,9 +63,9 @@ def _gen_datetime():
 
     From this gist: https://gist.github.com/rg3915/db907d7455a4949dbe69
     """
-    min_year = 2012
-    max_year = datetime.now().year
-    start = datetime(min_year, 11, 1, 00, 00, 00)
+    min_year = 2011
+    max_year = 2021
+    start = datetime(min_year, 1, 1, 00, 00, 00)
     years = max_year - min_year + 1
     end = start + timedelta(days=365 * years)
     return start + (end - start) * random.random()
